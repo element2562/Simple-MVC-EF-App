2c868638decd2a5e21509081a53973055f799b05
element2562 <element25668@gmail.com>
2018-11-29 13:35:09 -0600

    Oops I lied, seriously this time

---
+++ WebApplication5/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication5.Models;
+
+namespace WebApplication5.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Student> Student { get; set; }
+
+    }
+}
---
+++ WebApplication5/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication5.Models
+{
+    public class Student
+    {
+
+        [Key]
+        public int StudentId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        
+        [Range(0, 5)]
+        public double GPA { get; set; }
+
+    }
+}
